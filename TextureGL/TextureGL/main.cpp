fb4ea13d4c1919860e07b4c547ebd73489d60452
HJX <lkysyzxz@outlook.com>
2018-10-15 13:06:38 +0800

    save image as png|| fix load bug

--- TextureGL/TextureGL/main.cpp
+++ TextureGL/TextureGL/main.cpp
@@ -6,25 +6,11 @@
 #include "texture_gl.h"
 using namespace std;
 int main() {
-	int width, height, nrComponents;
-	unsigned char *data = stbi_load("x.png", &width, &height, &nrComponents, 0);
-	string l((const char*)(data+ 1440000));
-	if (data) {
-		cout << "R" << "\t" << "G" << "\t" << "B"<<endl;
-		//for (int i = 599; i < height; i++) {
-		//	for (int j = 799; j < width; j++) {
-		//		int R = data[(i*width + j)*3];
-		//		int G = data[(i*width + j) * 3 +1];
-		//		int B = data[(i*width + j) * 3 +2];
-		//		//char A = data[(i*width + j) * 3 + 3];
-		//		cout << R << "\t" << G << "\t" << B <<endl;
-		//	}
-		//}
-		int R = data[(0 * width + 799) * 3];//255
-		int G = data[(0 * width + 799) * 3 + 1];//255
-		int B = data[(0 * width + 799) * 3 + 2];//255
-		cout << R << "\t" << G << "\t" << B <<endl;
-	}
-	stbi_image_free(data);
+	//int width, height, nrComponents;
+	//nsigned char *data = stbi_load("x.png", &width, &height, &nrComponents, 0);
+	TextureGL texture("x.png");
+	texture.SaveImageAsPng("y");
+	
+	//stbi_image_free(data);
 	return 0;
 }
--- TextureGL/TextureGL/texture_gl.h
+++ TextureGL/TextureGL/texture_gl.h
@@ -1,6 +1,8 @@
 #ifndef TEXTURE_GL_H
 #define TEXTURE_GL_H
 #include "stb_image.h"
+#include "stb_image_write.h"
+#include <string>
 #include <iostream>
 struct Color {
 	float r;
@@ -16,7 +18,7 @@ private:
 	int numComponents;
 public:
 	TextureGL(const char *filename) {
-		this->data = (filename, &this->width, &this->height, &this->numComponents, 0);
+		this->data = stbi_load(filename, &this->width, &this->height, &this->numComponents, 0);
 		if (!data) {
 			std::cout << "Load image " << filename << " faile." << std::endl;
 		}
@@ -27,6 +29,9 @@ public:
 		res.r = data[(w*width + h) * 3];
 		res.g = data[(w*width + h) * 3+1];
 		res.b = data[(w*width + h) * 3+2];
+		res.r /= 255.0f;
+		res.g /= 255.0f;
+		res.b /= 255.0f;
 		return res;
 	}
 
@@ -35,7 +40,22 @@ public:
 		int h = height * v;
 		return GetColor(w, h);
 	}
+
+	unsigned int GetWidth() { return this->width; }
+
+	unsigned int GetHeight() { return this->height; }
+
+	void SetColor(int w, int h,Color color) {
+		data[(w*width + h) * 3] = 255 * color.r;
+		data[(w*width + h) * 3 + 1] = 255 * color.g;
+		data[(w*width + h) * 3 + 2] = 255 * color.b;
+	}
 	
+	int SaveImageAsPng(std::string filename) {
+		filename += ".png";
+		return stbi_write_png(filename.c_str(), this->width, this->height, 
+			this->numComponents, &(this->data[0]), 0);
+	}
 
 	~TextureGL() {
 		stbi_image_free(this->data);
