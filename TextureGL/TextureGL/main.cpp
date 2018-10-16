2ed6500b31ed92d88caff92e4ad7e73032237e88
HJX <lkysyzxz@outlook.com>
2018-10-16 23:02:06 +0800

    nothing

--- TextureGL/TextureGL/main.cpp
+++ TextureGL/TextureGL/main.cpp
@@ -8,15 +8,17 @@ using namespace std;
 int main() {
 	//int width, height, nrComponents;
 	//nsigned char *data = stbi_load("x.png", &width, &height, &nrComponents, 0);
-	TextureGL texture("water_map.png");
+	TextureGL texture("x.png");
 	cout << "R" << "\t" << "G" << "\t" << "B" << endl;
+	Color color{ 0,1,0 };
 	for (int h = 0;h< texture.GetHeight(); h++) {
 		for (int w = 0;w< texture.GetWidth(); w++) {
-			Color color = texture.GetColor(w, h);
-			cout << color.r << "\t" << color.g << "\t" << color.b << endl;
+			//Color color = texture.GetColor(w, h);
+			//cout << color.r << "\t" << color.g << "\t" << color.b << endl;
+			texture.SetColor(w, h, color);
 		}
 	}
-	texture.SaveImageAsPng("y");
+	texture.SaveImageAsPng("water_bump.png");
 	
 	//stbi_image_free(data);
 	return 0;
